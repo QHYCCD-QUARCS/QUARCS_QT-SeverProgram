bd90b2a72e134f2ddecbd1e6f3425494c55e9899
CJQ <QUARCS@qhyccd.com>
2025-11-12 17:34:52 +0800

    修改：     src/autopolaralignment.cpp

--- src/autopolaralignment.cpp
+++ src/autopolaralignment.cpp
@@ -2063,7 +2063,7 @@ bool PolarAlignment::waitForCaptureComplete()
     
     connect(&checkTimer, &QTimer::timeout, [&]() {
         if (isCaptureEnd) { // 5秒后假设完成
-            if (true) {
+            if (false) {
                 if (currentState == PolarAlignmentState::CHECKING_POLAR_POINT) {
                     lastCapturedImage = QString("/home/quarcs/workspace/QUARCS/testimage1/1.fits");
                 }
